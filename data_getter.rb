d2f5e95292598ba361a63521d5b6dbba4a5cc019
vagrant <vagrant@precise64.(none)>
2013-06-13 17:27:17 +0000

    cleaned up scrapping code

--- data_getter.rb
+++ data_getter.rb
@@ -5,8 +5,6 @@ require 'open-uri'
 
  File.open('scrapped2.txt', 'w') do |f|
       page = Nokogiri::HTML(open("http://www.interestingfacts.net/"))
-      #results = page.css("#middle table.adcontent.tblClpsePad table.viewad_frame_tbl #attributeTable td").text
-      #results = page.css(".postcontent ol").first
        results = page.css(".postcontent ol li")
        results.each do |whatever|
         what = (whatever.text + "\n")
